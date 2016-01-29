37726bd8a71d6a10e783f9242c58b9be99e47dd7
Ryan Jung <GradysGhost>
2016-01-29 15:31:49 -0700

    Move UHR to init; turn off maint mode

--- unite/core/UniteHttpResponse.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-class UniteHttpResponse:
-    def __init__(self, status_code=200, headers={}, body=None):
-        self.status_code = status_code
-        self.headers = headers
-        self.body = body
